6ca38c2098fa1472cd1192c91a651781fe1b1f04
Desik Rengarajan <desik.rengarajan@ecelbwi00204.engr.tamu.edu>
2022-02-02 11:32:22 -0600

    adding scripts file

--- logo/2_run_logo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source activate bail
-python run_logo.py --env-num 3 --high-kl 0.01 --seed 1
-python run_logo.py --env-num 3 --high-kl 0.01 --seed 2
-python run_logo.py --env-num 3 --high-kl 0.01 --seed 11
-python run_logo.py --env-num 5 --high-kl 0.01 --seed 11
-python run_logo.py --env-num 7 --high-kl 0.01 --seed 1
-python run_logo.py --env-num 7 --high-kl 0.01 --seed 2
-python run_logo.py --env-num 7 --high-kl 0.01 --seed 11
