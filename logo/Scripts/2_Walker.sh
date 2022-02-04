bbc482e29abb7a5ccb0c6e8a3c4e205aa570c44d
Desik Rengarajan <desik.rengarajan@ecelbwi00204.engr.tamu.edu>
2022-02-04 13:24:02 -0600

    scrpt changes

--- logo/Scripts/2_Walker.sh
+++ logo/Scripts/2_Walker.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 cd ..
 source activate bail
-python run_logo.py --env-num 5 --seed 1  --init-BC --gpu-index 1
-python run_logo.py --env-num 5 --seed 2  --init-BC --gpu-index 1
-python run_logo.py --env-num 5 --seed 3  --init-BC --gpu-index 1
-python run_logo.py --env-num 5 --seed 4  --init-BC --gpu-index 1
-python run_logo.py --env-num 5 --seed 5  --init-BC --gpu-index 1
-python run_logo.py --env-num 6 --seed 4  --gpu-index 1
-python run_logo.py --env-num 6 --seed 5  --gpu-index 1
+python run_logo.py --env-num 5 --seed 11  --gpu-index 1
+python run_logo.py --env-num 5 --seed 12  --gpu-index 1
+python run_logo.py --env-num 5 --seed 13  --gpu-index 1
+python run_logo.py --env-num 5 --seed 14  --gpu-index 1
+python run_logo.py --env-num 5 --seed 15  --gpu-index 1
