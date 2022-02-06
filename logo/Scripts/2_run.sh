2d70a2339c4feba7585ec1841379c2d33e1a2d47
Desik Rengarajan <desik.rengarajan@ecelbwi00204.engr.tamu.edu>
2022-02-05 18:18:28 -0600

    Adding more scrpt files

--- logo/Scripts/2_run.sh
+++ logo/Scripts/2_run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd ..
 source activate bail
-python run_logo.py --env-num 1 --seed 7  --gpu-index 0
-python run_logo.py --env-num 1 --seed 7  --gpu-index 0 --init-BC
-python run_logo.py --env-num 2 --seed 7  --gpu-index 0
-python run_logo.py --env-num 3 --seed 7  --gpu-index 0
-python run_logo.py --env-num 3 --seed 7  --gpu-index 0 --init-BC
-python run_logo.py --env-num 4 --seed 7  --gpu-index 0
-python run_logo.py --env-num 5 --seed 7  --gpu-index 0
-python run_logo.py --env-num 5 --seed 7  --gpu-index 0 --init-BC
-python run_logo.py --env-num 6 --seed 7  --gpu-index 0
-python run_logo.py --env-num 7 --seed 7  --gpu-index 0
-python run_logo.py --env-num 7 --seed 7  --gpu-index 0 --init-BC
-python run_logo.py --env-num 8 --seed 7  --gpu-index 0
+python run_logo.py --env-num 1 --seed 5  --gpu-index 0
+python run_logo.py --env-num 1 --seed 5  --gpu-index 0 --init-BC
+python run_logo.py --env-num 2 --seed 5  --gpu-index 0
+python run_logo.py --env-num 3 --seed 5  --gpu-index 0
+python run_logo.py --env-num 3 --seed 5  --gpu-index 0 --init-BC
+python run_logo.py --env-num 4 --seed 5  --gpu-index 0
+python run_logo.py --env-num 5 --seed 5  --gpu-index 0
+python run_logo.py --env-num 5 --seed 5  --gpu-index 0 --init-BC
+python run_logo.py --env-num 6 --seed 5  --gpu-index 0
+python run_logo.py --env-num 7 --seed 5  --gpu-index 0
+python run_logo.py --env-num 7 --seed 5  --gpu-index 0 --init-BC
+python run_logo.py --env-num 8 --seed 5  --gpu-index 0
